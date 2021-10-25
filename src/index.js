bab24e6d3bfdb416bd1680bf7cfade2ea492eb15
Dima Tytenko <amgpetronass@gmail.com>
2021-10-25 21:50:30 +0300

    add

--- src/index.js
+++ src/index.js
@@ -42,8 +42,8 @@ async function fetchPhotos() {
       return;
     } else {
       appendPhotosMarkup(hits);
-      removeIsHiddenBtn();
       scrollToButton(button);
+      removeIsHiddenBtn();
     }
   } catch (error) {
     console.log(error);
